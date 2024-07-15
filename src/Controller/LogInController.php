4d03314881f0f275c61e98d6c320272bbb00e2c5
xin <botella.xin@gmail.com>
2024-07-15 11:34:36 +0200

    add LoginForm

--- src/Controller/LogInController.php
+++ src/Controller/LogInController.php
@@ -2,33 +2,38 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\LoginFormType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LogInController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    #[Route('/login', name: 'login')]
+    public function login(Request $request,AuthenticationUtils $authenticationUtils): Response
     {
-        // // 如果用户已登录，重定向到仪表板或其他页面
-        // if ($this->isGranted('ROLE_USER')) {
-        //     return $this->redirectToRoute('dashboard');
-        // }
+        // 如果用户已登录，重定向到主页
         if ($this->getUser()) {
             return $this->redirectToRoute('home');
         }
-
-        // get the login error if there is one
+        // 获取登录错误信息（如果有）
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
+        // 获取最后输入的用户名
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('log_in/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
-    }
+        // 创建登录表单
+        $form = $this->createForm(LoginFormType::class, [
+            'username' => $lastUsername,
+        ]);
 
-    #[Route(path: '/logout', name: 'app_logout')]
+        return $this->render('log_in/login.html.twig', [
+            'form' => $form->createView(),
+            'error' => $error,
+        ]);    }
+
+    #[Route('/logout', name: 'logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
---
+++ src/Form/LoginFormType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class LoginFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class,  [
+                'label' => 'Email',
+                'attr' => [
+                    'class' => 'w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500',
+                    'placeholder' => 'Enter your email',
+                ],
+                'label_attr' => [
+                    'class' => 'block text-gray-600', // Add your label class here
+                ],
+            ])
+
+            ->add('password', PasswordType::class, [
+                'label' => 'Password',
+                'attr' => [
+                    'class' => 'w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500',
+                    'placeholder' => 'Enter your email',
+                ],
+                'label_attr' => [
+                    'class' => 'block text-gray-600', // Add your label class here
+                ],
+            ])
+            ->add('remember', CheckboxType::class, [
+                'label' => 'Remember Me',
+                'required' => false,
+                'attr' => [
+                    'class' => 'text-blue-500',
+                ],
+                'label_attr' => [
+                    'class' => 'text-gray-600 ml-2',
+                ],
+            ]);
+           
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
